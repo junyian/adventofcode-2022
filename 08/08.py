7d391ddeaaaba9df9cb3ef488377abb15b5473bb
Lean Jun Yian <junyian@gmail.com>
2022-12-08 09:43:03 +0000

    08 - redoing counting of visible trees

--- 08/08.py
+++ 08/08.py
@@ -1,4 +1,22 @@
 import numpy
+import sys
+
+def countvisible(tree, line):
+    retval = 0
+    lasttree = tree
+    for i, v in enumerate(line):
+        if v > lasttree:
+            retval += 1
+            break
+    return retval
+
+print(countvisible(5, [2])) # 1
+print(countvisible(5, [5, 2])) # 1
+print(countvisible(1, [5, 5, 2])) # 1
+print(countvisible(3, [3, 5, 6]))
+print(countvisible(3, [3, 5, 2]))
+print(countvisible(5, [3, 3, 2]))
+sys.exit(0)
 
 lines = open("input1.txt").readlines()
 
@@ -16,7 +34,7 @@ for w in range(height):
 for w in range(height):
     for h in range(width):
         if w==0 or h==0 or w==width-1 or h==width-1:
-            masks1[w][h] = 1
+            masks1[h][w] = 1
         else:
             tree = trees[w][h]
 
@@ -24,63 +42,56 @@ for w in range(height):
             col = trees[:,h]
 
             left = trees[w,:h]
-            print(tree, row, left)
             if left.max() < tree:
                 masks1[w][h] = 1
+            # print(tree, row, left)
             
-            score = 0
-            if h>1:
-                for l in left[::-2]:
-                    # print(l)
-                    if l >= tree:
-                        break
-                    else:
-                        score += 1
-            masks2[w][h] = score+1
+            print(tree, row, left[::-1])
+            masks2[w][h] = countvisible(tree, left[::-1])
 
-            right = trees[w,h+1:]
-            # print(tree, row, right)
-            if right.max() < tree:
-                masks1[w][h] = 1
+            # right = trees[w,h+1:]
+            # # print(tree, row, right)
+            # if right.max() < tree:
+            #     masks1[w][h] = 1
             
-            score = 0
-            if h<width-1:
-                for l in right[:-1]:
-                    if l >= tree:
-                        break
-                    else:
-                        score += 1
+            # score = 0
+            # if h<width-1:
+            #     for l in right[:-1]:
+            #         if l >= tree:
+            #             break
+            #         else:
+            #             score += 1
             # masks2[i][j] *= score + 1
 
-            top = trees[:w,h]
+            # top = trees[:w,h]
             # print(tree, col, top)
-            if top.max() < tree:
-                masks1[w][h] = 1
+            # if top.max() < tree:
+            #     masks1[w][h] = 1
 
-            score = 0
-            if w>1:
-                for l in top[::-1]:
-                    if l >= tree:
-                        break
-                    else:
-                        score += 1
+            # score = 0
+            # if w>1:
+            #     for l in top[::-1]:
+            #         if l >= tree:
+            #             break
+            #         else:
+            #             score += 1
             # masks2[i][j] *= score + 1
 
-            bottom = trees[w+1:,h]
+            # bottom = trees[w+1:,h]
             # print(tree, col, bottom)
-            if bottom.max() < tree:
-                masks1[w][h] = 1
+            # if bottom.max() < tree:
+            #     masks1[w][h] = 1
 
-            score = 0
-            if w<height-1:
-                for l in bottom[:-1]:
-                    if l >= tree:
-                        break
-                    else:
-                        score += 1
-            masks2[w][h] *= score + 1
+            # score = 0
+            # if w<height-1:
+            #     for l in bottom[:-1]:
+            #         if l >= tree:
+            #             break
+            #         else:
+            #             score += 1
+            # masks2[w][h] *= score + 1
 print(masks1)
 print(masks1.sum())
 
-print(masks2[1])
+print(masks2)
 print(masks2.max())
