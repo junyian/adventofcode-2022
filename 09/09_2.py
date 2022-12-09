0b1665ac714920bbc65fd6a068612c74cfbe6c87
Lean Jun Yian <junyian@gmail.com>
2022-12-09 15:11:02 +0000

    09 - commit to sync to local

--- 09/09_2.py
+++ 09/09_2.py
@@ -1,16 +1,17 @@
+lines = open("input1.txt").readlines()
 class Rope:
     def __init__(self):
         self.h_row, self.t_row, self.h_col, self.t_col = 0, 0, 0, 0
         self.paths = set()
     
-    def movehead(self, dir):
-        if dir=='L':
+    def movehead(self, direction):
+        if direction=='L':
             self.h_col -= 1
-        elif dir=='R':
+        elif direction=='R':
             self.h_col += 1
-        elif dir=='U':
+        elif direction=='U':
             self.h_row -= 1
-        elif dir=='D':
+        elif direction=='D':
             self.h_row += 1
     
     def movetail(self):
@@ -30,12 +31,41 @@ class Rope:
             self.t_row -= 1                                         # ..
             self.t_col -= 1                                         # .T
         
+        elif self.t_row==self.h_row+1 and self.t_col==self.h_col-2: # H..
+            self.t_row -= 1                                         # ..T
+            self.t_col -= 1
+
         elif self.t_row==self.h_row+2 and self.t_col==self.h_col-1: # .H
             self.t_row -= 1                                         # ..
             self.t_col += 1                                         # T.
         
-        elif self.t_row==self.h_row+1 and self.t_col==self.h_col-2: # H..
-            self.t_row -= 1                                         # ..T
+        elif self.t_row==self.h_row+1 and self.t_col==self.h_col-2: # ..H
+            self.t_row -= 1                                         # T..
+            self.t_col += 1
+        
+        elif self.t_row==self.h_row-2 and self.t_col==self.h_col-1: # .T
+            self.t_row += 1                                         # ..
+            self.t_col += 1                                         # H.
+        
+        elif self.t_row==self.h_row-1 and self.t_col==self.h_col-2: # ..T
+            self.t_row += 1                                         # H..
             self.t_col -= 1
-            
-            
+        
+        elif self.t_row==self.h_row-2 and self.t_col==self.h_col+1: # T.
+            self.t_row += 1                                         # ..
+            self.t_col += 1                                         # .H
+        
+        elif self.t_row==self.h_row-1 and self.t_col==self.h_col+2: # T..
+            self.t_row += 1                                         # ..H
+            self.t_col += 1
+        
+        self.paths.add((self.t_row, self.t_col))
+
+rope = Rope()
+
+for l in lines:
+    dir, count = l.strip().split(' ')
+    for i in range(int(count)):
+        rope.movehead(dir)
+        rope.movetail()
+print(len(rope.paths))
