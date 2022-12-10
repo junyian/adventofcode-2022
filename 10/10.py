cb008c71fc27286df50aa65dda0eef0caf7d505f
junyian <junyian@gmail.com>
2022-12-11 00:17:17 +0800

    10 - final

--- 10/10.py
+++ 10/10.py
@@ -6,6 +6,7 @@ class CPU:
         self.cycle = 0
         self.queue = [(None,None)]
         self.score = 0
+        self.crt = ''
 
     def next_cycle(self):
         instruction = self.queue.pop(0)
@@ -14,8 +15,11 @@ class CPU:
         else:
             pass
         self.cycle += 1
-        self.check_cycle()
-    
+        self.check_cycle() # Part 1
+        self.draw_crt()    # Part 2
+        cpu.print_crt()
+        # input()
+
     def check_cycle(self):
         if (self.cycle - 20) % 40 == 0:
             self.print_cycle()
@@ -28,8 +32,22 @@ class CPU:
             self.queue.append((None, None))
             self.queue.append(('addx', arg))
 
+    def draw_crt(self):
+        if (self.cycle%40-1) >= (self.reg_X-1) and (self.cycle%40-1) <= (self.reg_X+1):
+            self.crt += '#'
+        else:
+            self.crt += '.'
+
     def print_cycle(self):
         print(f"Cycle {self.cycle}: X={self.reg_X} SS={self.cycle * self.reg_X}")
+    
+    def print_crt(self):
+        print(self.crt[0:40])
+        print(self.crt[40:80])
+        print(self.crt[80:120])
+        print(self.crt[120:160])
+        print(self.crt[160:200])
+        print(self.crt[200:240])
 
 cpu = CPU()
 
@@ -37,7 +55,7 @@ cpu = CPU()
 # cpu.queue_instruction('addx', 3)
 # cpu.queue_instruction('addx', -5)
 
-for l in open("input1.txt").readlines():
+for l in open("input2.txt").readlines():
     if l.startswith('noop'):
         cpu.queue_instruction('noop')
     elif l.startswith('addx'):
