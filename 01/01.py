1e43a1d555981ab1cf8cad9516abaa8cc500e66b
junyian <junyian@gmail.com>
2022-12-14 00:38:09 +0800

    01 - cleanup

--- 01/01.py
+++ 01/01.py
@@ -1,16 +1,13 @@
-f = open("input2.txt").readlines()
-
-elves = []
-c = 0
-for i in f:
-    calories = i.strip()
-    if len(calories) > 0:
-        c = c + int(calories)
-    else:
-        elves.append(c)
-        c = 0
-elves.append(c)
-print(max(elves))
-
-elves.sort(reverse=True)
-print(elves[0] + elves[1] + elves[2])
+with open("input2.txt") as f:
+    elves = []
+    c = 0
+    for i in f:
+        calories = i.strip()
+        if len(calories) > 0:
+            c = c + int(calories)
+        else:
+            elves.append(c)
+            c = 0
+    elves.append(c) # the last row
+    elves.sort(reverse=True)
+    print((max(elves), sum(elves[:3])))
