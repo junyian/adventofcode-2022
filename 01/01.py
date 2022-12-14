2bdc034e4bb8298a7e52036eda8498279aac933f
junyian <junyian@gmail.com>
2022-12-14 22:34:26 +0800

    01 - refactor

--- 01/01.py
+++ 01/01.py
@@ -1,13 +1,18 @@
-with open("input2.txt") as f:
-    elves = []
-    c = 0
-    for i in f:
-        calories = i.strip()
-        if len(calories) > 0:
-            c = c + int(calories)
-        else:
-            elves.append(c)
-            c = 0
-    elves.append(c) # the last row
-    elves.sort(reverse=True)
-    print((max(elves), sum(elves[:3])))
+def solve(input:str):
+    with open(input) as f:
+        elves = []
+        c = 0
+        for i in f:
+            calories = i.strip()
+            if len(calories) > 0:
+                c = c + int(calories)
+            else:
+                elves.append(c)
+                c = 0
+        elves.append(c) # the last row
+        elves.sort(reverse=True)
+        return (max(elves), sum(elves[:3]))
+
+if __name__ == "__main__":
+    print(solve("input1.txt"))
+    print(solve("input2.txt"))
